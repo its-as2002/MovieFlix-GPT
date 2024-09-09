2f359f6c6dc735553e0e501c815e998ea8564699
Ayush Srivastava <its.as2002@gmail.com>
2024-09-10 05:09:42 +0530

    update - added form validations

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "./Header";
 import { useState } from "react";
 import { LOGIN_BACKGROUND_URL } from "../Utils/constants";
+import { validateForm } from "../Utils/validateForm";
 
 const Login = () => {
-	const [signInForm, setSignInForm] = useState(false);
-	const handleToggel = () => {
+	const [signInForm, setSignInForm] = useState(true);
+	const [errorMsg, setErrorMsg] = useState("");
+	const email = useRef("");
+	const password = useRef(null);
+	const name = useRef(null);
+	const handleToggel = (e) => {
 		setSignInForm(!signInForm);
 	};
+	const handleButtonClick = () => {
+		const message = validateForm(
+			!signInForm && name.current.value ? name.current.value : "",
+			email.current.value,
+			password.current.value
+		);
+		setErrorMsg(message);
+	};
+
 	return (
 		<div className="relative">
 			<Header />
@@ -15,36 +29,47 @@ const Login = () => {
 				<img
 					src={LOGIN_BACKGROUND_URL}
 					alt="bg-image"
-					className="object-cover"
+					className="object-cover w-full h-screen"
 				/>
 			</div>
 			<div
 				id="form"
 				className="absolute top-1/4 left-1/2 -translate-x-1/2 px-16 py-2  bg-black bg-opacity-70  ">
-				<form className="flex flex-col opacity-500 ">
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+					}}
+					className="flex flex-col opacity-500 ">
 					<p className="font-bold text-3xl text-white my-7">
 						{signInForm ? "Sign In" : "Sign Up"}
 					</p>
 					{!signInForm && (
 						<input
-							className="py-4 px-4 w-80 my-2 rounded-md bg-opacity-60 text-white bg-black"
+							ref={name}
+							className="py-4 px-4 w-80 my-2 rounded-md border border-gray-500 bg-opacity-60 text-white bg-black"
 							type="text"
 							placeholder="Name"
+							required
 						/>
 					)}
 					<input
-						className="py-4 px-4 w-80 my-2 rounded-md bg-opacity-60 text-white bg-black"
+						ref={email}
+						className="py-4 px-4 w-80 my-2 rounded-md bg-opacity-60 border border-gray-500 text-white bg-black"
 						type="text"
 						placeholder="Email or phone number"
+						required
 					/>
 					<input
-						className="py-4 px-4 w-80 my-2 rounded-md bg-opacity-60 text-white bg-black"
+						ref={password}
+						className="py-4 px-4 w-80 my-2 rounded-md bg-opacity-60 border border-gray-500 text-white bg-black"
 						type="password"
 						placeholder="Password"
+						required
 					/>
+					{errorMsg && <p className="text-red-500 text-sm">{errorMsg}</p>}
 					<button
 						type="submit"
-						onClick={handleToggel}
+						onClick={handleButtonClick}
 						className="px-5 py-2 my-5 rounded-md bg-[#E50914] text-white font-semibold">
 						{signInForm ? "Sign In" : "Sign Up"}
 					</button>
---
+++ src/Utils/validateForm.js
@@ -0,0 +1,14 @@
+export const validateForm = (name, emailOrMobile, password) => {
+	const isNameValid = /^[A-Za-z]+([A-Za-z]+)*/.test(name);
+	const isEmailValid = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailOrMobile);
+	const isMobileValid = /^[0-9]{10}$/.test(emailOrMobile);
+	const isPasswordValid = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\W]{8,}$/.test(
+		password
+	); 
+    if(name === "" || emailOrMobile === "" || password === "") return "All fields are required";
+	if (name !== "" && !isNameValid) return "Name is invalid";
+	if (!isEmailValid && !isMobileValid) return "Email or Mobile is invalid";
+	if (!isPasswordValid) return "Password is invalid";
+
+	return "";
+};
